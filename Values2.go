b7ab1dd07788adf5a3aa198223434484267d4283
zhuchao <zhuchao@benmu-health.com>
2018-07-06 14:51:16 +0800

    example

---
+++ Values2.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var strA = "init"
+	fmt.Println(strA)
+	var a, b int = 0, 1
+	fmt.Println(a, b)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "var and init"
+	fmt.Println(f)
+}
