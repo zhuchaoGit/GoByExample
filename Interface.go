1235b8bb42847f13d23305e9c2d7836c5c123aea
zhuchao <zhuchao@benmu-health.com>
2018-07-10 16:05:10 +0800

    example

--- Interface.go
+++ Interface.go
@@ -8,10 +8,12 @@ import (
 type GS interface {
 	Get() int
 	Set(int)
+	GetDouble() int
 }
 
 type G interface {
 	Get() int
+	GetDouble() int
 }
 
 type S struct {
@@ -24,14 +26,19 @@ func (s *S) Get() int {
 func (s *S) Set(i int) {
 	s.val = i
 }
+func (s *S) GetDouble() int {
+	return s.val * 2
+}
 
 func fgs(gs GS) {
 	gs.Set(1)
 	fmt.Println("gs:", reflect.TypeOf(gs).String(), gs.Get())
+	fmt.Println("double gs:", reflect.TypeOf(gs).String(), gs.GetDouble())
 }
 
 func fg(g G) {
 	fmt.Println("g:", reflect.TypeOf(g).String(), g.Get())
+	fmt.Println("double g:", reflect.TypeOf(g).String(), g.GetDouble())
 }
 
 func main() {
