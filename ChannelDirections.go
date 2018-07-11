9841a58d061545209f27c4daf5f7936488fa9b5a
zhuchao <zhuchao@benmu-health.com>
2018-07-11 11:53:30 +0800

    example

--- ChannelDirections.go
+++ ChannelDirections.go
@@ -6,12 +6,13 @@ func ping(pings chan<- string, msg string) {
 	pings <- msg
 }
 func pong(pings <-chan string, pongs chan<- string) {
+	//msg := <-pings
 	pongs <- <-pings
 }
 
 func main() {
-	pings := make(chan string)
-	pongs := make(chan string)
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
 	ping(pings, "hello")
 	pong(pings, pongs)
 	fmt.Println(<-pongs)
