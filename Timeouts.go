c001cfb68b7cdda254da448be694b0e3429b10be
zhuchao <zhuchao@benmu-health.com>
2018-07-12 16:43:20 +0800

    example

--- Timeouts.go
+++ Timeouts.go
@@ -1,11 +1,31 @@
 package main
 
-import "time"
+import (
+	"time"
+	"fmt"
+)
 
 func main() {
-	c1 := make(chan string)
+	c1 := make(chan string, 1)
 	go func() {
 		time.Sleep(2 * time.Second)
 		c1 <- "message"
 	}()
+	select {
+	case msg := <-c1:
+		fmt.Println("message", msg)
+	case <-time.After(1 * time.Second):
+		fmt.Println("out time")
+	}
+	c2 := make(chan string, 1)
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "message"
+	}()
+	select {
+	case msg := <-c2:
+		fmt.Println("message", msg)
+	case <-time.After(3 * time.Second):
+		fmt.Println("out time")
+	}
 }
