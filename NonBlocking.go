2548f274d8b39144d989db741f0e5038720d5125
zhuchao <zhuchao@benmu-health.com>
2018-07-12 16:54:17 +0800

    example

---
+++ NonBlocking.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	messages := make(chan string, 1)
+	signals := make(chan bool, 1)
+
+	select {
+	case msg := <-messages:
+		fmt.Println("receieved message", msg)
+	default:
+		fmt.Println("no message")
+	}
+
+	msg := "hello"
+	select {
+	case messages <- msg:
+		fmt.Println("send hello")
+	default:
+		fmt.Println("no send")
+	}
+	select {
+	case msg := <-messages:
+		fmt.Println("recieved message", msg)
+	case sig := <-signals:
+		fmt.Println("recieved signal", sig)
+	default:
+		fmt.Println("nothing happened")
+	}
+}
